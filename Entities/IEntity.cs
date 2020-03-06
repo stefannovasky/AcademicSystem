1751c957514abc7e0dfe4f68086e03e43f143452
miguelSchuldt <miguelizador@gmail.com>
2020-03-06 13:44:20 -0300

    first commit

---
+++ Entities/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    internal interface IEntity
+    {
+        public int ID { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Entities
+{
+    public class User : IEntity
+    {
+        public int ID { get; set; }
+        public bool IsActive { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
