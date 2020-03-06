1a5434a0f5f331b8a18c6a38a75e1b8aa0f67a53
miguelSchuldt <miguelizador@gmail.com>
2020-03-06 17:04:35 -0300

    DbSets e DbContext criados

---
+++ DAL/AcademyContext.cs
@@ -0,0 +1,34 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DAL
+{
+    public class AcademyContext : DbContext
+    {
+        public AcademyContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+        public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Coordinator> Coordinators { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Evaluation> Evaluations { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
