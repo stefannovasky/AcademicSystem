cb32d58c630e0a8d1fef8c4d560930fb8a7a8874
miguelschuldt <miguelizador@gmail.com>
2020-03-07 15:23:34 -0300

    connthere

--- DAL/AcademyContext.cs
+++ DAL/AcademyContext.cs
@@ -15,7 +15,7 @@ namespace DAL
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            
+            optionsBuilder.UseSqlServer(@"");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
