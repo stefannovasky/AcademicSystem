3c1d198c937d0df609f1386a34663ade39a670a5
miguelschuldt <miguelizador@gmail.com>
2020-03-07 13:55:46 -0300

    userMapConfig OK

--- DAL/MapConfigs/UserMapConfig.cs
+++ DAL/MapConfigs/UserMapConfig.cs
@@ -11,7 +11,25 @@ namespace DAL.MapConfigs
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
-            builder.Property(c => c.Cpf).HasMaxLength(14).IsRequired();
+            builder.Property(u => u.Email).HasMaxLength(100).IsRequired();
+            builder.HasIndex(u => u.Email).IsUnique(true);
+
+            builder.Property(u => u.Password).HasMaxLength(100).IsRequired();
+            
+            builder.Property(u => u.Cpf).IsRequired();
+            builder.HasIndex(u => u.Cpf).IsUnique(true);
+
+            builder.Property(u => u.Rg).HasMaxLength(16).IsRequired();
+
+            builder.Property(u => u.City).HasMaxLength(100).IsRequired();
+
+            builder.Property(u => u.Street).HasMaxLength(120).IsRequired();
+
+            builder.Property(u => u.Number).HasMaxLength(10).IsRequired();
+
+            builder.Property(u => u.State).HasMaxLength(25).IsRequired();
+
+            builder.Property(u => u.Name).HasMaxLength(200);
         }
     }
 }
