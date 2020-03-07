0cac30cbf8e99050b2b3b96cb5311637006bd6d3
miguelschuldt <miguelizador@gmail.com>
2020-03-07 16:29:15 -0300

    Entidades associativas adiconadas

---
+++ Entities/StudentCourse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class StudentCourse
+    {
+        public int StudentID { get; set; }
+        public Student Student { get; set; }
+        public int CourseID { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ Entities/SubjectInstructor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class SubjectInstructor
+    {
+        public int SubjectID { get; set; }
+        public Subject Subject{ get; set; }
+        public int InstructorID { get; set; }
+        public Instructor Instructor { get; set; }
+    }
+}
