1c1b9ac99ac7cdc6256e212bcf106729a3ef8865
unknown <stefannovasky@gmail.com>
2020-03-07 14:55:17 -0300

    repositories and impl added

--- DAL/Impl/AttendanceRepository.cs
+++ DAL/Impl/AttendanceRepository.cs
@@ -9,5 +9,29 @@ namespace DAL.Impl
 {
     public class AttendanceRepository : IAttendanceRepository
     {
+        public Response Create(Attendance item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataResponse<Attendance> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataResponse<Attendance> GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataResponse<Attendance> Update(Attendance item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- DAL/Impl/ClassRepository.cs
+++ DAL/Impl/ClassRepository.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using DAL.Interfaces;
+using Entities;
+using Shared;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DAL.Impl
 {
-    class ClassRepository
+    public class ClassRepository : IClassRepository
     {
+        public Response Create(Class item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataResponse<Class> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataResponse<Class> GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataResponse<Class> Update(Class item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
