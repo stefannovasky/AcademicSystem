c7120af1fccfbe5d7c865600f8d476607bfaf1d8
stefannovasky <stefannovasky@gmail.com>
2020-03-09 17:09:21 -0300

    HashUtils added

---
+++ Shared/HashUtils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Shared
+{
+    public class HashUtils
+    {
+        public string HashString(string text)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public bool CompareHash(string text, string hash)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                text = this.HashString(text);
+
+                return text == hash; 
+            }
+        }
+    }
+}
---
+++ Testes/TestHashUtils.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    class TestHashUtils
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void ShouldTextEncryptedEqualHash()
+        {
+            string text = "stefan novasky";
+
+            string encrypted = new HashUtils().HashString(text);
+
+            bool res = new HashUtils().CompareHash(text, encrypted);
+
+            Assert.IsTrue(res);
+        }
+    }
+}
