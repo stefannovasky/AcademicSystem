9a4c216df9a0966e281a916321b0a029345e0b68
stefannovasky <stefannovasky@gmail.com>
2020-03-09 16:35:44 -0300

    UserService OK

--- BLL/Impl/UserService.cs
+++ BLL/Impl/UserService.cs
@@ -1,5 +1,8 @@
 ﻿using BLL.Interfaces;
+using BLL.Validators;
+using DAL.Impl;
 using Entities;
+using FluentValidation.Results;
 using Shared;
 using System;
 using System.Collections.Generic;
@@ -10,29 +13,99 @@ namespace BLL.Impl
 {
     public class UserService : IUserService
     {
+        private UserRepository _userRepo = new UserRepository();
+
         public async Task<Response> Create(User item)
         {
-            throw new NotImplementedException();
+            Response response = new Response();
+            try
+            {
+                ValidationResult validationResponse = new UserValidator().Validate(item);
+                if (!validationResponse.IsValid)
+                {
+                    response.Success = false;
+                    response.ErrorList.Add("Validation error");
+                    return response;
+                }
+
+                response = await _userRepo.Create(item);
+                return response;                
+            }
+            catch (Exception)
+            {
+                response.ErrorList.Add("Error on create user");
+                response.Success = false; 
+                return response; 
+            }
         }
 
         public async Task<Response> Delete(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Response response = await _userRepo.Delete(id);
+                return response;
+            }
+            catch (Exception)
+            {
+                Response r = new Response() { Success = false };
+                r.ErrorList.Add("Error on delete user");
+                return r; 
+            }
         }
 
         public async Task<DataResponse<User>> GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                DataResponse<User> response = await _userRepo.GetAll();
+                return response; 
+            }
+            catch (Exception)
+            {
+                DataResponse<User> r = new DataResponse<User>() { Success = false };
+                r.ErrorList.Add("Error on read users");
+                return r;
+            }
         }
 
         public async Task<DataResponse<User>> GetByID(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                DataResponse<User> response = await _userRepo.GetByID(id);
+                return response;
+            }
+            catch (Exception)
+            {
+                DataResponse<User> r = new DataResponse<User>() { Success = false };
+                r.ErrorList.Add("Error on get user");
+                return r;
+            }
         }
 
         public async Task<DataResponse<User>> Update(User item)
         {
-            throw new NotImplementedException();
+            DataResponse<User> response = new DataResponse<User>();
+            try
+            {
+                ValidationResult validationResponse = new UserValidator().Validate(item);
+                if (!validationResponse.IsValid)
+                {
+                    response.Success = false;
+                    response.ErrorList.Add("Validation error");
+                    return response;
+                }
+
+                response = await _userRepo.Update(item);
+                return response;
+            }
+            catch (Exception)
+            {
+                response.ErrorList.Add("Error on update user");
+                response.Success = false;
+                return response;
+            }
         }
     }
 }
---
+++ BLL/Validators/UserValidator.cs
@@ -0,0 +1,24 @@
+﻿using Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+		{
+			RuleFor(x => x.Name).Length(0, 100);
+			RuleFor(x => x.Email).EmailAddress();
+			RuleFor(x => x.Cpf).Length(14);
+            RuleFor(x => x.Password).Length(8, 100);
+            RuleFor(x => x.Rg).Length(8, 16);
+            RuleFor(x => x.Street).Length(1, 120);
+            RuleFor(x => x.Number).Length(0, 6);
+            RuleFor(x => x.State).Length(3);
+            RuleFor(x => x.City).Length(1, 120);
+		}
+	}
+}
