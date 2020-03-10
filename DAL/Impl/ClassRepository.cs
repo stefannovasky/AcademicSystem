b1b663f07835378f8fa0956cbdb98823781596f1
unknown <stefannovasky@gmail.com>
2020-03-10 19:12:19 -0300

    ClassRepo OK

--- DAL/Impl/ClassRepository.cs
+++ DAL/Impl/ClassRepository.cs
@@ -142,5 +142,105 @@ namespace DAL.Impl
                 return response;
             }
         }
+
+        public async Task<Response> AddStudent(Class Class, Student student)
+        {
+            Response response = new Response();
+            try
+            {
+                using (AcademyContext context = new AcademyContext())
+                {
+                    StudentClass studentClass = new StudentClass()
+                    {
+                        ClassID = Class.ID,
+                        StudentID = student.ID
+                    };
+                    (await context.Classes.Include(c => c.Students).Where(c => c.ID == Class.ID).FirstOrDefaultAsync()).Students.Add(studentClass);
+                    await context.SaveChangesAsync();
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+                response.Success = false;
+                response.ErrorList.Add("Error while addind student to class.");
+                return response;
+            }
+        }
+
+        public async Task<Response> AddEvaluation(Class Class, Evaluation evaluation)
+        {
+            Response response = new Response();
+            try
+            {
+                using (AcademyContext context = new AcademyContext())
+                {
+                    EvaluationClass evaluationClass = new EvaluationClass()
+                    {
+                        ClassID = Class.ID,
+                        EvaluationID = evaluation.ID
+                    };
+                    (await context.Classes.Include(c => c.Evaluations).Where(c => c.ID == Class.ID).FirstOrDefaultAsync()).Evaluations.Add(evaluationClass);
+                    await context.SaveChangesAsync();
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+                response.Success = false;
+                response.ErrorList.Add("Error while addind evaluation to class.");
+                return response;
+            }
+        }
+
+        public async Task<Response> AddAttendance(Class Class, Attendance attendance)
+        {
+            Response response = new Response();
+            try
+            {
+                using (AcademyContext context = new AcademyContext())
+                {
+                    AttendanceClass attendanceClass = new AttendanceClass()
+                    {
+                        ClassID = Class.ID,
+                        AttendanceID = attendance.ID
+                    };
+                    (await context.Classes.Include(c => c.Attendances).Where(c => c.ID == Class.ID).FirstOrDefaultAsync()).Attendances.Add(attendanceClass);
+                    await context.SaveChangesAsync();
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+                response.Success = false;
+                response.ErrorList.Add("Error while addind attendance to class.");
+                return response;
+            }
+        }
+
+        public async Task<Response> AddCoordinator(Class Class, Coordinator coordinator)
+        {
+            Response response = new Response();
+            try
+            {
+                using (AcademyContext context = new AcademyContext())
+                {
+                    CoordinatorClass coordinatorClass = new CoordinatorClass()
+                    {
+                        ClassID = Class.ID,
+                        CoordinatorID = coordinator.ID
+                    };
+                    (await context.Classes.Include(c => c.Coordinators).Where(c => c.ID == Class.ID).FirstOrDefaultAsync()).Coordinators.Add(coordinator);
+                    await context.SaveChangesAsync();
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+                response.Success = false;
+                response.ErrorList.Add("Error while addind coordinator to class.");
+                return response;
+            }
+        }
     }
 }
---
+++ Entities/EvaluationClass.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class EvaluationClass
+    {
+        public int EvaluationID { get; set; }
+        public Evaluation Evaluation { get; set; }
+        public int ClassID { get; set; }
+        public Class Class { get; set; }
+    }
+}
