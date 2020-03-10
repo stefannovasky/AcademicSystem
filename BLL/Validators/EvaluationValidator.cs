bcd7c895701053a92ac495cc963375321243c312
unknown <stefannovasky@gmail.com>
2020-03-09 21:48:00 -0300

    validators ok

---
+++ BLL/Validators/EvaluationValidator.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Validators
+{
+    public class EvaluationValidator : AbstractValidator<Evaluation>
+    {
+        public EvaluationValidator()
+        {
+            RuleFor(x => x.Name).Length(1, 100);
+        }
+    }
+}
---
+++ BLL/Validators/SubjectValidator.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Validators
+{
+    public class SubjectValidator : AbstractValidator<Subject>
+    {
+        public SubjectValidator()
+        {
+            RuleFor(x => x.Name).Length(1, 120);
+        }
+    }
+}
