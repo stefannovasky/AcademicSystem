1e93bfee5c382644af2c35416206e968536baed8
unknown <stefannovasky@gmail.com>
2020-03-10 20:27:32 -0300

    Repos OK

--- DAL/Interfaces/ICourseRepository.cs
+++ DAL/Interfaces/ICourseRepository.cs
@@ -1,11 +1,16 @@
 ﻿using Entities;
+using Shared;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DAL.Interfaces
 {
     public interface ICourseRepository : IRepository<Course>
     {
+        Task<Response> AddOwner(Course course, Owner owner);
+        Task<Response> AddSubject(Course course, Subject subject);
+        Task<Response> AddClass(Course course, Class Class);
     }
 }
