20714562d68e5afe9173ea7e54e07ee3f2b2a70d
miguelschuldt <miguelizador@gmail.com>
2020-03-15 12:59:19 -0300

    StudentController getStudent OK

--- SistemaAcademico/Controllers/StudentController.cs
+++ SistemaAcademico/Controllers/StudentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using AcademicSystemApi.Extensions;
 using BLL.Interfaces;
 using Entities;
 using Microsoft.AspNetCore.Authorization;
@@ -17,9 +18,19 @@ namespace AcademicSystemApi.Controllers
     public class StudentController : ControllerBase
     {
         IStudentService _service;
-        public StudentController(IStudentService service)
+        IUserService userService;
+        IInstructorService InstructorService;
+        ICoordinatorService CoordinatorService;
+        IOwnerService OwnerService;
+        ICourseService CourseService;
+        public StudentController(IStudentService service, IUserService userService, IInstructorService InstructorService, ICoordinatorService coordinatorService, IOwnerService ownerService, ICourseService courseService)
         {
             this._service = service;
+            this.userService = userService;
+            this.InstructorService = InstructorService;
+            this.CoordinatorService = coordinatorService;
+            this.OwnerService = ownerService;
+            this.OwnerService = ownerService;
         }
 
         [Authorize]
@@ -48,8 +59,62 @@ namespace AcademicSystemApi.Controllers
         {
             try
             {
-                DataResponse<Student> response = await _service.GetByID(id);
-                response.Data[0].User.Student = null;
+                bool isPermited = false;
+                User user = (await userService.GetByID(this.GetUserID())).Data[0];
+                DataResponse<Student> studentResponse = (await _service.GetByID(id));
+                if (studentResponse.Success)
+                {
+                    Student student = studentResponse.Data[0];
+                    if (user.Student != null && user.Student.ID == id)
+                    {
+                        isPermited = true;
+                    }
+                    if (user.Instructor != null)
+                    {
+                        Instructor instructor = (await InstructorService.GetByID(user.Instructor.ID)).Data[0];
+                        foreach (InstructorClass instructorClass in instructor.Classes)
+                        {
+                            if (student.Classes.Where(ic => ic.ClassID == instructorClass.ClassID).Count() > 0)
+                            {
+                                isPermited = true;
+                            }
+                        }
+                    }
+                    if (user.Coordinator != null)
+                    {
+                        Coordinator Coordinator = (await CoordinatorService.GetByID(user.Coordinator.ID)).Data[0];
+
+                        foreach (CoordinatorClass CoordinatorClass in Coordinator.Classes)
+                        {
+                            if (student.Classes.Where(ic => ic.ClassID == CoordinatorClass.ClassID).Count() > 0)
+                            {
+                                isPermited = true;
+                            }
+                        }
+                    }
+                    if (user.Owner != null)
+                    {
+                        Owner Owner = (await OwnerService.GetByID(user.Owner.ID)).Data[0];
+                        foreach (OwnerCourse ownerCourse in Owner.Courses)
+                        {
+                            Course course = (await CourseService.GetByID(ownerCourse.CourseID)).Data[0];
+                            foreach (Class @class in course.Classes)
+                            {
+                                if (student.Classes.Where(sc => sc.ClassID == @class.ID).Count() > 0)
+                                {
+                                    isPermited = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (isPermited)
+                {
+                    return studentResponse;
+                }
+                DataResponse<Student> response = new DataResponse<Student>();
+                response.Success = false;
+                response.ErrorList.Add("Permission Denied");
                 return response;
             } catch (Exception e)
             {
